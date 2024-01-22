b8b6dafcb290c3197c0125dd24bae5ce85a2a1d4
kazi331 <kazisharifulislam52@gmail.com>
2024-01-22 23:19:12 +0600

    Add Access-Control-Allow-Credentials header to login response

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -98,6 +98,9 @@ const login = async (req, res) => {
 
         // create token
         const token = await user.generateJWT();
+        // set header
+        res.header('Access-Control-Allow-Credentials', true);
+        // res.setHeader('Content-Type', 'text/html'); // nextjs 
         // save token in cookie
         res.cookie('token', 'Bearer ' + token, cookieConfig);
         // save user in cookie
