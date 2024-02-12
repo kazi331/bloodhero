ab2445184788545cb3e89a748bb3bc64d4353bdb
Kazi Shariful Islam <kazisharifulislam52@gmail.com>
2024-02-12 12:15:11 +0600

    package.json changed

--- server/config/urls.js
+++ server/config/urls.js
@@ -1,3 +1,3 @@
 export const clientURI = process.env.NODE_ENV === 'development' ? process.env.CLIENT_URL : process.env.CLIENT_URL_PROD
 // export const mongoURI = process.env.NODE_ENV === 'development' ? process.env.MONGO_URI : process.env.MONGO_URI_PROD
-export const mongoURI = process.env.MONGO_URI_PRO
+export const mongoURI = process.env.MONGO_URI_PROD
