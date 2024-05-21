68035cba0c910447c3b6c05a72f15ce5adafff12
kazi331 <kazisharifulislam52@gmail.com>
2024-05-21 20:55:57 +0600

    social login layout changed

--- client/app/(frontend)/(auth)/SocialLogin.tsx
+++ client/app/(frontend)/(auth)/SocialLogin.tsx
@@ -3,6 +3,7 @@ import { GoogleIcon } from '@/components/ui/icons';
 import { serverURI } from '@/lib/utils';
 import { Button } from '@components/ui/button';
 import { Github } from 'lucide-react';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 const SocialLogin = () => {
@@ -12,16 +13,19 @@ const SocialLogin = () => {
     }
 
     return (
-        <div className='flex flex-col gap-y-2'>
-            <Button onClick={() => loginWith('google')} className="w-full text-gray-700 " variant="outline">
-                <GoogleIcon className="h-5 w-5 mr-2" />
-                Login with Google
+        <div className='flex gap-x-2'>
+            <Button className="w-full text-gray-700  " variant="outline" onClick={() => loginWith("google")}>
+                {/* <GoogleIcon className="h-5 w-5 mr-2" /> */}
+                <Image width={20} height={24} src="/brand/google.svg" alt="google" className='h-auto w-auto' />
             </Button>
-            <Button onClick={() => loginWith('github')} className="w-full text-gray-700 " variant="outline">
-                <Github className="h-5 w-5 mr-2" />
-                Login with Github
+            <Button className="w-full text-gray-700  " variant="outline" onClick={() => loginWith("facebook")}>
+                <Image width={20} height={24} src="/brand/facebook.svg" alt="facebook" className='h-auto w-auto' />
+                {/* <FacebookIcon className="h-5 w-5 mr-2 text-gray-500" /> */}
             </Button>
-
+            <Button className="w-full text-gray-700  " variant="outline" onClick={() => loginWith("apple")}>
+                {/* <AppleIcon className="h-6 w-6 mr-2" /> */}
+                <Image width={20} height={24} src="/brand/apple.svg" alt="apple" className='h-auto w-auto' />
+            </Button>
         </div>
     )
 }
