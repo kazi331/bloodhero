71cfbb880e2baa5243e4d7c77216245d998bae3c
nazmulhasanrakib <163629409+nazmulhasanrakib@users.noreply.github.com>
2024-05-22 11:34:26 +0600

    added Sharif word in noty

--- client/app/(frontend)/(mainRoutes)/notifications/page.tsx
+++ client/app/(frontend)/(mainRoutes)/notifications/page.tsx
@@ -8,6 +8,7 @@ const Page = () => {
             <Header />
             <div className="min-h-[calc(100vh-7rem)] flex items-center justify-center">
                 <h2>Notifications</h2>
+                <p>Sharif</p>
             </div>
         </div>
     )
