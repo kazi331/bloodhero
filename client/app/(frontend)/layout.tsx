d0a6f51a508f3745fe8af9e464739f4aeaf495bb
kazi331 <kazisharifulislam52@gmail.com>
2024-05-22 19:46:00 +0600

    loader added

--- client/app/(frontend)/layout.tsx
+++ client/app/(frontend)/layout.tsx
@@ -2,6 +2,8 @@
 import BottomNav from '@/components/home/BottomNav';
 import { AuthProvider } from "@/context/authContext";
 import { Galada, Hind_Siliguri } from 'next/font/google';
+import NextTopLoader from 'nextjs-toploader';
+
 
 import React from 'react';
 import { Toaster } from 'sonner';
@@ -32,6 +34,7 @@ export default function FrontLayout({ children, }: { children: React.ReactNode }
                     <BottomNav />
                 </div>
             </div >
+            <NextTopLoader color='hsl(344deg 80% 56% / 70%)' />
         </AuthProvider>
     )
 }
