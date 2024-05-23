cc3fca9e806201373b48674293d055cce51d3386
kazi331 <kazisharifulislam52@gmail.com>
2024-05-23 23:43:37 +0600

    chore: Add 'https://roktimaloy.org' to allowed origins in CORS configuration

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors(
     {
-        origin: ['http://localhost:3000', 'https://bloodhero.vercel.app'],
+        origin: ['http://localhost:3000', 'https://bloodhero.vercel.app', 'https://roktimaloy.org'],
         credentials: true,
     }
 ))
